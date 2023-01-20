0079143b03caf4f97771eb002d6bfae26f466001
AlanATR <AlanATR@users.noreply.github.com>
2023-01-20 20:44:27 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="AlanATR"
+original_name="gender_classifier"
+original_urlname="gender_classifier"
+original_description="Awesome gender_classifier created by AlanATR"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv gender_classifier $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
--- tests/test_base.py
+++ tests/test_base.py
@@ -1,5 +1,5 @@
-from project_name.base import NAME
+from gender_classifier.base import NAME
 
 
 def test_base():
-    assert NAME == "project_name"
+    assert NAME == "gender_classifier"
